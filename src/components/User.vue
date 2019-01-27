693c5cc8d9371b57b5af47ad85d36b0683539f38
Aman <rumman.djuice1@gmail.com>
2019-01-27 22:57:06 +0100

    first commit

--- src/components/User.vue
+++ src/components/User.vue
@@ -2,10 +2,12 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button v-on:click="changeName">Change my Name</button>
+        <p>The name in Parent is: {{name}}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail v-bind:myName="name" v-on:coming-from-child="name=$event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -22,6 +24,16 @@
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
+        },
+        data:function(){
+            return{
+              name:'Aman',
+            };
+        },
+        methods:{
+            changeName(){
+                this.name='Roza';
+            }
         }
     }
 </script>
--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,10 +2,30 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name in child : {{myName}}</p>
+        <p>{{splitName()}}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
+    export default{
+        props:{
+            myName:{
+                type:String,
+                default:'Abbu'
+            },
+        },
+        methods:{
+            splitName(){
+                return this.myName.split("").reverse().join("");
+            },
+            resetName(){
+                this.myName='Aman'; //this change is local only for this child component
+                this.$emit('coming-from-child', this.myName);
+            }
+        }
+    }
 </script>
 
 <style scoped>
